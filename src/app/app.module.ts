a7b135da154d5694f0f8bc6fb9c55a0add438d63
Candela Salvatto <cande_salvatto12@hotmail.com>
2023-01-15 23:18:29 -0300

    seg commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Section1Component } from './components/section1/section1.component';
+import { Section2Component } from './components/section2/section2.component';
+import { Section3Component } from './components/section3/section3.component';
+import { SectionSliderComponent } from './components/section-slider/section-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    SectionSliderComponent
   ],
   imports: [
     BrowserModule
